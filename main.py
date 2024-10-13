c686048095ed004451226a32533b111897a4be2d
Gabriel-Chevallier <gabrielchevallier7@gmail.com>
2024-10-13 11:05:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import socket
-s = socket.socket(); s.bind(('0.0.0.0', 12345)); s.listen(2); c1, c2 = s.accept(), s.accept()
-while True: c2[0].send(c1[0].recv(1024)); c1[0].send(c2[0].recv(1024))
+
+s = socket.socket()
+s.bind(('0.0.0.0', 10000))  # Utiliser le port 10000 pour Render
+s.listen(2)
+c1, c2 = s.accept(), s.accept()
+while True: 
+    c2[0].send(c1[0].recv(1024)); 
+    c1[0].send(c2[0].recv(1024))
